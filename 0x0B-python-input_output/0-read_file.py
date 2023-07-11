9555c0c644e9bf85f0dff46882819a3ebb1ff1e7
waleed0116755216 <waleedraslan1989@gmail.com>
2023-07-11 16:56:01 -0700

    1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""Module containing the function read_file"""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    """Reads a file and prints to stdout.
+
+    Args:
+        filename (str, optional): name of file to read. Defaults to "".
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
